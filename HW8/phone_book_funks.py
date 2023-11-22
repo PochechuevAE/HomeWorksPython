a8ab5b6cca50f1292dc0278c54670b39c46bc92c
Pochechuev <trakturus@list.ru>
2023-11-23 02:02:06 +0300

    at HW8_pbf add view, delet  and edit funcs

--- HW8/phone_book_funks.py
+++ HW8/phone_book_funks.py
@@ -16,3 +16,39 @@ def save_contacts(contacts):
 def add_contact(contacts, data):
     contacts.append({"Фамилия": data[0], "Имя": data[1], "Отчество": data[2], "Номер телефона": data[3]})
     save_contacts(contacts)
+    
+def view_all_contacts(contacts):
+    if not contacts:
+        msgbox("Справочник пуст", 'Телефонный справочник')
+        return
+
+    msg = ""
+    for contact in contacts:
+        msg += f"{contact['Фамилия']} {contact['Имя']} {contact['Отчество']}: {contact['Номер телефона']}\n"
+    
+    msgbox(msg, 'Телефонный справочник')
+    
+def delete_contact(contacts, index):
+    contact = contacts[index]
+    confirmation = ynbox(f"Вы уверены, что хотите удалить контакт:\n{contact['Фамилия']} {contact['Имя']} {contact['Отчество']}?", 'Удаление контакта')
+
+    if confirmation:
+        del contacts[index]
+        save_contacts(contacts)
+        msgbox("Контакт успешно удален!")
+        
+def edit_contact(contacts, index):
+    contact = contacts[index]
+    msg = "Измените контакт"
+    title = "Карточка контакта"
+    field_names = ["Фамилия", "Имя", "Отчество", "Номер телефона"]
+    field_values = multenterbox(msg, title, field_names, 
+                                values=[contact['Фамилия'], contact['Имя'], 
+                                        contact['Отчество'], contact['Номер телефона']])
+
+    if field_values:
+        contacts[index] = {"Фамилия": field_values[0], 
+                           "Имя": field_values[1], "Отчество": field_values[2], 
+                           "Номер телефона": field_values[3]}
+        save_contacts(contacts)
+        msgbox("Контакт успешно изменен!")
