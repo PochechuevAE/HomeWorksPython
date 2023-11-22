d929fc29d46ccae4f806c5dbeaf97a1c2b7ae857
Pochechuev <trakturus@list.ru>
2023-11-23 01:59:37 +0300

    at HW8_pb add while True and add contact

--- HW8/phone_book.py
+++ HW8/phone_book.py
@@ -4,3 +4,18 @@ from phone_book_funks import *
 contacts = load_contacts()
 
 msgbox("Вас приветствует Телефонный справочник")
+
+while True:
+    choices = ['Добавить контакт', 'Просмотреть все контакты', 
+               'Изменить контакт', 'Удалить контакт','Выход']
+    choice = buttonbox("Выберите действие", "Телефонный справочник", choices)
+
+    if choice == 'Добавить контакт':
+        msg = "Введите контакт"
+        title = "Карточка контакта"
+        field_names = ["Фамилия", "Имя", "Отчество", "Номер телефона"]
+        field_values = multenterbox(msg, title, field_names)
+        
+        if field_values:
+            add_contact(contacts, field_values)
+            msgbox("Контакт успешно добавлен!")
